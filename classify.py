a71cd2078613405eaf01fe0fe3be7a47456d9332
Cyril Stoller <cyril.stoller@gmail.com>
2017-04-11 19:02:35 +0200

    fixed bug in manual classification exception situation

--- classify.py
+++ classify.py
@@ -17,7 +17,12 @@ vocab = pickle.load(open('scratch/vocab.pkl', 'rb'))
 clf = joblib.load('scratch/classifier.joblib.pkl')
 
 test_tweeds = []
-undeterminable = {4590:1}
+undeterminable = {867:1, 868:1, 869:1, 899:1, 993:1, 1881:1, 2128:1, 3219:1,
+3588:1, 3765:1, 3777:1, 3822:-1, 4213:1, 4590:1, 4948:-1, 5314:1, 5327:-1,
+5871:1, 5898:1, 6150:1, 7119:1, 7120:1, 7349:1, 7398:-1, 7529:-1, 7530:-1,
+8093:1, 8094:1, 8113:1, 8859:1, 9290:1, 9502:1}
+
+print('Starting to classify test set')
 
 with open('datasets/test_data.txt') as f:
     for line in f:
@@ -27,16 +32,18 @@ with open('datasets/test_data.txt') as f:
         tokens = [vocab.get(t, -1) for t in tweed.strip().split()]
         tokens = [t for t in tokens if t >= 0]
         if not tokens:
-            print(id)
-            label = undeterminable[id]
+            if id in undeterminable:
+                label = undeterminable[id]
             print("couldn't assign tweed '{}' a feature. Manually: {}".format(tweed.strip(), label))
         else:
             vectors = np.array([xs[t] for t in tokens])
             label = clf.predict(np.average(vectors, axis=0).reshape(1, -1))
         test_tweeds.append([id, label])
 
+
 # write classifications to csv file
 timestamp = '{:%Y-%m-%d_%H%M%S}'.format(datetime.datetime.now())
+print('Finished classication. Saving csv file to submissions/submission_{}.csv'.format(timestamp))
 if not os.path.exists('submissions/'):
     os.makedirs('submissions/')
 np.savetxt('submissions/submission_{}.csv'.format(timestamp), test_tweeds, delimiter=",", fmt='%d', header='Id,Prediction', comments='')
